19fc41e3281ef1c0107370f8989b1de868ae9eb9
zrisher <zrisher@yahoo.com>
2013-02-10 01:00:05 -0800

    debug login page issues

--- linkedin_login_page.php
+++ linkedin_login_page.php
@@ -46,7 +46,11 @@ $oauthc->setNonce(rand());
 
 if(empty($_SESSION['oauth_lrequest_secret'])) {
     //get the request token and store it
-    $request_token_info = $oauthc->getRequestToken($config['oauth']['requesttokenurl'],$callback_url);
+    try {
+    $request_token_info = $oauthc->getRequestToken($config['oauth']['requesttokenurl'],$callback_url);}
+    catch (exception $e){
+        print_xml($oauthc->getLastResponse());
+    }
     $_SESSION['oauth_lrequest_secret'] = $request_token_info['oauth_token_secret'];
     $_SESSION['oauth_lrequest_token'] = $request_token_info['oauth_token'];
     //forward user to authorize url
